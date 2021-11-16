c14b00f1750aa16063c3bc3775b33b2e5401f7d3
carlos <carlos.caserotto@yahoo.com>
2021-11-16 10:54:57 -0300

    Add User db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Pili", password: "pili")
+User.create(username: "Carli", password: "carli")
+User.create(username: "Caro", password: "caro")
+User.create(username: "Mama", password: "mama")
+User.create(username: "Colo", password: "colo")
+
