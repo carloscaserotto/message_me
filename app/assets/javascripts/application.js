3c618a30540b471784f7bc7594188f4be21160d8
carlos <carlos.caserotto@yahoo.com>
2021-11-17 11:45:26 -0300

    Ready to deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,8 +23,8 @@ scroll_bottom = function () {
   }
 }
 
-submit_message = function(){
-  $('message_body').on('keydown', function(e) {
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
     if (e.keyCode == 13) {
       $('button').click()
       e.target.value = "";
